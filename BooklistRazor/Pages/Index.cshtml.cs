cf2b20414fc2721e456d12e2633cd56f15206e75
Andrew Wilson <atwilson0729@gmail.com>
2024-01-28 15:37:09 -0500

    Added SQL Server connection to appsettings.json
    Added several Nuget packages (may need installed on other machines):
    Razor.RuntimeCompilation
    EntityFrameworkCore
    EntityFrameworkCore.SqlServer
    EntityFrameworkCore.Tools

--- BooklistRazor/Pages/Index.cshtml.cs
+++ BooklistRazor/Pages/Index.cshtml.cs
@@ -12,9 +12,9 @@ namespace BooklistRazor.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return Page();  
         }
     }
 }
